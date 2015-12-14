ceebf20913e2605fdf9ca48e7eb84a7d3cc288f4
Cameron Browning <camb416@gmail.com>
2015-12-14 12:33:05 -0500

    added folders

---
+++ dirs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+touch addons/.gitkeep
+touch communication/.gitkeep
+touch empty/.gitkeep
+touch events/.gitkeep
+touch gl/.gitkeep
+touch graphics/.gitkeep
+touch gui/.gitkeep
+touch math/.gitkeep
+touch sound/.gitkeep
+touch utils/.gitkeep
+touch video/.gitkeep
