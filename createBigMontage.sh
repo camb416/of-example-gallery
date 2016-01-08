cec20964e3bb73dab93030a205eaedcac30a254e
Cameron Browning <camb416@gmail.com>
2016-01-08 17:57:16 -0500

    added script to make big montage

---
+++ createBigMontage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir montages
+cp 3d/montage.png montages/3d.png
+cp addons/montage.png montages/addons.png
+cp communication/montage.png montages/communication.png
+cp empty/montage.png montages/empty.png
+cp events/montage.png montages/events.png
+cp gl/montage.png montages/gl.png
+cp graphics/montage.png montages/graphics.png
+cp gui/montage.png montages/gui.png
+cp math/montage.png montages/math.png
+cp sound/montage.png montages/sound.png
+cp utils/montage.png montages/utils.png
+cp video/montage.png montages/video.png
